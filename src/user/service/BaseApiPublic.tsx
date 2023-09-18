d49b9fb9ec2f63923c79d501649e4c2d11f912fa
ducnv2509 <ducit2509@gmail.com>
2023-09-18 16:54:59 +0700

    ducnvg

--- src/user/service/BaseApiPublic.tsx
+++ src/user/service/BaseApiPublic.tsx
@@ -1,3 +1,3 @@
-const base_url_public = "http://180.93.175.236:30003/api";
-// const base_url_public = "http://localhost:3002/api";
+const base_url_public = "http://localhost:3002/api";
+// const base_url_public = "http://103.196.145.232:6801/api/v1";
 export default base_url_public;
